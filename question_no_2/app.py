2affb62577fd1a37042261613cd571d55a5feab5
Nikita <patil.nikita830.gmail.com>
2026-03-27 15:54:58 +0530

    updated comments adding

--- question_no_2/app.py
+++ question_no_2/app.py
@@ -1,16 +1,18 @@
 
 #2. Create a new branch named <your_name>_new (e.g., Tutedude_new).
-#      nikita_new
+#                git branch nikita_new
 #Update the content of the JSON file used for the /api route in this branch.
-# updated contect of JSON files
+#                updated contect of JSON files
+#                git add app.py
+#                git commit -m "app.py"
+#                git push 
 #Merge the <your_name>_new branch into the main branch.
+#                git switch master
+#                git merge nikita_new
+#                git push
 #If there are conflicts during the merge, resolve them by accepting the changes from the <your_name>_new branch.
 #Add the resolved changes to the staging area, commit them, and push the updates to the remote repository.
-
-
-#1. Create a Flask application with an /api route. When this route is accessed, 
-#it should return a JSON list. The data should be stored in a backend file, read from it, 
-#and sent as a response. 
+#               there was no conflict as I created new repo
 
 
 from flask import Flask, render_template, request
